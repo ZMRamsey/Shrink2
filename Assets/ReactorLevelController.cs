59eb57de84835ec5af8051dfbea3db04aa830c07
Zoey Ramsey <32360184+ZMRamsey@users.noreply.github.com>
2019-11-07 11:04:32 +0000

    quick push

--- Assets/ReactorLevelController.cs
+++ Assets/ReactorLevelController.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 public class ReactorLevelController : MonoBehaviour
 {
     public GameObject vent;
+    public GameObject entranceVent;
     public Light spot1;
     public Light spot2;
+    public Light plateLight1;
+    public Light plateLight2;
     public PressurePlate plate1;
     public PressurePlate plate2;
     public LightPulse reactor;
+    public float ventPush;
 
     // Start is called before the first frame update
     void Start()
     {
         spot2.intensity = 0;
-
     }
 
     // Update is called once per frame
@@ -24,13 +27,30 @@ public class ReactorLevelController : MonoBehaviour
         if (plate1.GetState())
         {
             spot2.intensity = 4.55f;
-            vent.transform.Translate(vent.transform.position + (Vector3.forward * 1.5f));
+            vent.GetComponent<Rigidbody>().AddForce(Vector3.up * ventPush);
+            vent.GetComponent<Rigidbody>().AddForce(Vector3.back * ventPush);
+            plateLight1.intensity = 3.0f;
+            plateLight1.color = Color.red;
             if (plate2.GetState())
             {
                 spot1.intensity = 0.0f;
                 spot2.intensity = 0.0f;
+                plateLight2.intensity = 3.0f;
+                plateLight2.color = Color.red;
                 reactor.Finale();
+                //Voice line
+                //Delay
+                entranceVent.GetComponent<Rigidbody>().AddForce(Vector3.up * ventPush * 3);
+                entranceVent.GetComponent<Rigidbody>().AddForce(Vector3.left * ventPush * 3);
             }
+            else
+            {
+                plateLight2.intensity += 0.01f;
+            }
+        }
+        else
+        {
+            plateLight1.intensity += 0.01f;
         }
     }
 }
